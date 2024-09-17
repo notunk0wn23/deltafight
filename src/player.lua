01664c7b06fa7f79620b1c6ede271c6264be101c
notunk0wn23 <73479844+notunk0wn23@users.noreply.github.com>
2024-09-17 09:38:09 -0500

    Engine work

---
+++ src/player.lua
@@ -0,0 +1,23 @@
+PlayerModule = {}
+
+function PlayerModule.new()
+    local player = setmetatable({}, PlayerModule)
+
+    player.x = love.graphics.getWidth() / 2
+    player.y = love.graphics.getHeight() / 2
+
+    player.speed = 200
+
+    player.img = love.graphics.newImage('assets/sprites/party/kris/dark/walk/down_1.png')
+
+    player.ground = player.y
+
+    player.y_velocity = -1
+
+    player.jump_height = -750
+    player.gravity = -5000
+
+    return player
+end
+
+return PlayerModule
